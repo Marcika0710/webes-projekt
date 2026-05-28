7f0ebc4dacffbade2a2422163fb6b318be3e8120
Marcika0710 <liptakmarcell22@gmail.com>
2026-05-28 15:42:52 +0200

    create the hamburger menu

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 use: "strict"
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('open');
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('open');
+    });
+});
